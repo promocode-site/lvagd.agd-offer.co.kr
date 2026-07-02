559045a8090098b0b5d6d399f4aa47ae05fc960c
minkyo-park <mkparkorea@gmail.com>
2026-07-02 22:31:42 +0900

    CSR to SSG 전환 (vite-react-ssg)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import type { RouteRecord } from "vite-react-ssg";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+import { Outlet } from "react-router-dom";
 import Index from "./pages/Index";
 import HowToUse from "./pages/HowToUse";
 import Comparison from "./pages/Comparison";
@@ -13,25 +13,31 @@ import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <HelmetProvider>
+function RootLayout() {
+  return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/how-to-use" element={<HowToUse />} />
-            <Route path="/comparison" element={<Comparison />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <ScrollToTop />
+        <Outlet />
       </TooltipProvider>
     </QueryClientProvider>
-  </HelmetProvider>
-);
+  );
+}
 
-export default App;
+export const routes: RouteRecord[] = [
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Index /> },
+      { path: "how-to-use", element: <HowToUse /> },
+      { path: "comparison", element: <Comparison /> },
+      { path: "faq", element: <FAQ /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+];
+
+export default routes;
